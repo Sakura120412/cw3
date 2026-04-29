0a70fd2103b1bb1edab68cc5615bc91ded3781e8
Sakura120412 <1744654925@qq.com>
2026-04-29 11:56:23 +0800

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,132 @@
+﻿const generatedBibEntries = {
+    "AlMalla2022Attention": {
+        "author": "Al-Malla, Mohammad A. and Jafar, A. and Ghneim, N.",
+        "doi": "10.1186/s40537-022-00571-w",
+        "journal": "Journal of Big Data",
+        "keywords": "attention, object-features, baseline",
+        "pages": "28",
+        "title": "Image Captioning Model Using Attention and Object Features to Mimic Human Image Understanding",
+        "type": "article",
+        "volume": "9",
+        "year": "2022"
+    },
+    "AlMalla2026Survey": {
+        "author": "Al-Malla, Mohammad A. and Hamdoun, O. and Ghneim, N.",
+        "doi": "10.1186/s40537-026-01377-w",
+        "journal": "Journal of Big Data",
+        "keywords": "survey, image-captioning, taxonomy, challenges",
+        "pages": "48",
+        "title": "A Comprehensive Survey on Deep Learning Approaches for Image Captioning: A Systematic Review",
+        "type": "article",
+        "volume": "13",
+        "year": "2026"
+    },
+    "Anderson2018BUTD": {
+        "author": "Anderson, Peter and He, Xiaodong and Buehler, Chris and Teney, Damien and Johnson, Mark and Gould, Stephen and Zhang, Lei",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR.2018.00636",
+        "keywords": "attention, region-features, baseline",
+        "pages": "6077--6086",
+        "title": "Bottom-Up and Top-Down Attention for Image Captioning and Visual Question Answering",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "Berger2025EvaluationSurvey": {
+        "author": "Berger, Uri and Stanovsky, Gabriel and Abend, Omri and Frermann, Lea",
+        "doi": "10.1162/tacl.a.52",
+        "journal": "Transactions of the Association for Computational Linguistics",
+        "keywords": "evaluation, survey, metrics, challenges",
+        "pages": "1597--1644",
+        "title": "Surveying the Landscape of Image Captioning Evaluation: A Comprehensive Taxonomy, Trends, and Metrics Analysis",
+        "type": "article",
+        "volume": "13",
+        "year": "2025"
+    },
+    "Cho2022FineCap": {
+        "author": "Cho, Jaemin and Yoon, Seongho and Kale, Abhinav and Dernoncourt, Franck and Bui, Trung and Bansal, Mohit",
+        "booktitle": "Findings of the Association for Computational Linguistics: NAACL 2022",
+        "doi": "10.18653/v1/2022.findings-naacl.39",
+        "keywords": "fine-grained, clip, reward, evaluation",
+        "pages": "517--527",
+        "title": "Fine-Grained Image Captioning with CLIP Reward",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "Jiang2019REO": {
+        "author": "Jiang, Mingyang and Hu, Jinhui and Huang, Qingqing and Zhang, Lei and Diesner, Jana and Gao, Jianfeng",
+        "booktitle": "Proceedings of the 2019 Conference on Empirical Methods in Natural Language Processing and the 9th International Joint Conference on Natural Language Processing",
+        "doi": "10.18653/v1/D19-1156",
+        "keywords": "evaluation, fine-grained, metrics",
+        "pages": "1485--1490",
+        "title": "REO-Relevance, Extraness, Omission: A Fine-Grained Evaluation for Image Captioning",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Ming2022Review": {
+        "author": "Ming, Y. and Hu, N. N. and Fan, C. X. and Feng, F. and Zhou, J. W. and Yu, H.",
+        "doi": "10.1109/JAS.2022.105734",
+        "journal": "IEEE/CAA Journal of Automatica Sinica",
+        "keywords": "survey, image-captioning, review",
+        "number": "8",
+        "pages": "1339--1365",
+        "title": "Visuals to Text: A Comprehensive Review on Automatic Image Captioning",
+        "type": "article",
+        "volume": "9",
+        "year": "2022"
+    },
+    "Rennie2017SCST": {
+        "author": "Rennie, Steven J. and Marcheret, Etienne and Mroueh, Youssef and Ross, Jerret and Goel, Vaibhava",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR.2017.131",
+        "keywords": "reinforcement-learning, optimization, baseline",
+        "pages": "1179--1195",
+        "title": "Self-Critical Sequence Training for Image Captioning",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "Sharma2023Taxonomy": {
+        "author": "Sharma, Himani and Padha, Deepika",
+        "doi": "10.1007/s10462-023-10488-2",
+        "journal": "Artificial Intelligence Review",
+        "keywords": "survey, taxonomy, open-issues",
+        "number": "11",
+        "pages": "13619--13661",
+        "title": "A Comprehensive Survey on Image Captioning: From Handcrafted to Deep Learning-Based Techniques, a Taxonomy and Open Research Issues",
+        "type": "article",
+        "volume": "56",
+        "year": "2023"
+    },
+    "Sharma2025PixelsToProse": {
+        "author": "Sharma, A. and Singh, H. and Pant, M.",
+        "doi": "10.1016/j.neucom.2025.132385",
+        "journal": "Neurocomputing",
+        "keywords": "survey, generative-ai, image-captioning",
+        "pages": "132385",
+        "title": "Pixels to Prose: A Comprehensive Survey of Image Captioning Techniques with Deep Learning and Generative Artificial Intelligence",
+        "type": "article",
+        "volume": "667",
+        "year": "2025"
+    },
+    "Wang2025Distinctive": {
+        "author": "Wang, J. and Xu, W. and Wang, Q. and others",
+        "doi": "10.1007/s11263-024-02220-6",
+        "journal": "International Journal of Computer Vision",
+        "keywords": "distinctive-captioning, attention, fine-grained",
+        "pages": "1435--1455",
+        "title": "Group-Based Distinctive Image Captioning with Memory Difference Encoding and Attention",
+        "type": "article",
+        "volume": "133",
+        "year": "2025"
+    },
+    "Ye2026KGC": {
+        "author": "Ye, Kailiang and Lu, Zheng and Shen, Linlin and Cui, Tianxiang",
+        "doi": "10.1016/j.eswa.2026.131405",
+        "journal": "Expert Systems With Applications",
+        "keywords": "fine-grained, contrastive-learning, reward, grammar",
+        "pages": "131405",
+        "title": "Rewarding Fine-Grained Image Captioning with Keyword Group Contrastive",
+        "type": "article",
+        "volume": "312",
+        "year": "2026"
+    }
+};
